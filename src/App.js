6898cc57c4a516052f1b7b10a47dcb6025cfe080
Chomo Gwom <chomogwom@gmail.com>
2019-04-24 02:53:50 +0200

    App can detect a face from a picture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
-import Particles from 'react-particles-js'
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+
+
+const app = new Clarifai.App({
+    apiKey: '74448d5657014f9ea8656877db501479'
+});
 
 const particlesOptions= {
     particles:{
@@ -31,9 +39,48 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            input: ''
+            input: '',
+            imageUrl: '',
+            box:''
         }
     }
+
+    calculateFaceLocation=(obj)=>{
+        const clarifaiFace = obj.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputImage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        console.log(obj);
+        return{
+
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)//algorithm starts calculates points from the top. So to get the face's bottom point, subtract the product of percentage bottom [CLARIFAIFACE.BOTTOM_ROW] and overall height of the image from the overall height.
+        }
+    }
+
+
+    displayFaceBox =(box)=>{
+        console.log(box);
+        this.setState({box:box});
+    }
+    onInputChange=(event)=>{
+        this.setState({input: event.target.value})
+    }
+
+    onButtonSubmit=()=>{
+
+        //set the state of the input box to contain the image URL
+        this.setState({imageUrl:this.state.input});
+
+        //make call to clarifai API by passing the model to use and the URL of the image, in the input box
+        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+            .then(response=> this.displayFaceBox(this.calculateFaceLocation(response)))//call calculateFaceLocation with received response to determine where the box will be placed, on the detected face. Then call displayFaceBox to add borders to the image
+            .catch(err=>console.log(err));
+    }
+
+
     render(){
       return (
         <div className="App">
@@ -41,9 +88,9 @@ class App extends Component {
           <Navigation />
           <Logo />
           <Rank />
-          <ImageLinkForm />
+          <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
 
-          {/*<FaceRecognition />*/}
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
         </div>
       );
     }
